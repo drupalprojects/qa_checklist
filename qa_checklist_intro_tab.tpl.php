f1381dfcd04c27c86867599edaec7289885d4d9e
Eidolon Night <helusionary@gmail.com>
2014-08-04 01:31:55 -0400

    Section descriptions and info update

--- qa_checklist_intro_tab.tpl.php
+++ qa_checklist_intro_tab.tpl.php
@@ -17,11 +17,11 @@
 
 <h4>Information</h4>
 
-<p>This module was written for Drupal site builders, developers, and System administrators or anyone else in charge of launching a Drupal site. The tasks within represent a comprehensive list of things to review and tasks to perform before going live. If you need help with Drupal SEO best practices, the search engines' latest changes, your brand's target audience, or strategic marketing objectives, consider using a Drupal development shop consultant like WebOzy or others listed HERE.</p>
+<p>This module was written for Drupal site builders, developers, and System administrators or anyone else in charge of launching a Drupal site. The tasks within represent a comprehensive list of things to review and tasks to perform before going live. If you need help, or this list doesn't make any sense to you, we suggest hiring a Drupal development shop like [WebOzy](http://webozy.com/) or others listed in the [Marketplace](https://www.drupal.org/drupal-services).</p>
 
 <h4>A bit about checklists</h4>
 
-<p>Each time you open the QAChecklist, it will look to see if any tasks have already been completed. For example, if you've already turned on clean URLs then that item will be checked. You still need to click "Save" to time and date stamp the automatically-checked items.</p>
+<p>Each time you open the QA Checklist, it will look to see if any tasks have already been completed. You still need to click "Save" to time and date stamp the automatically-checked items.</p>
 
 <p>The tasks here are designed to be done sequentially, from top to bottom.</p>
 
@@ -30,7 +30,8 @@
 <p>lorem</p>
 
 <h4>Don't do it all</h4>
-<p>Not every task in this list needs to, or even should, be done. Some of this will rely on your web host or PAAS provider. Using a Drupal-optimized provider such as Pantheon means that much of this list is already completed. Sit back and enjoy some iced tea. For standard web hosts, you may want to ask about the availability of certain points.</p>
+
+<p>Not every task in this list needs to, or even should, be done. Some of this will rely on your web host or PAAS provider. Using a Drupal-optimized provider such as [Pantheon](https://www.getpantheon.com/) or [Acquia](http://www.acquia.com/products-services/acquia-cloud) means that much of this list is already completed. Sit back and enjoy some iced tea. For standard web hosts, you may want to ask about the availability of certain features.</p>
 
 <h4>Credits</h4>
-<p>The Drupal QA Checklist was created by Nicholas Garofalo (Eidolon Night), the CTO of WebOzy, Inc. and other staff members. Development was paid for exclusively by Volacci. Special thanks to Travis Carden who created the Checklist API and ported the 7.x-1.x branch.</p>
+<p>The Drupal QA Checklist was created by [Nicholas Garofalo (Eidolon Night)](https://www.drupal.org/user/191336), the CTO of WebOzy, Inc. and other staff members. Special thanks to [Travis Carden](https://www.drupal.org/user/236758) who created the [Checklist API](https://www.drupal.org/project/checklistapi) and ported the 7.x-1.x branch.</p>
